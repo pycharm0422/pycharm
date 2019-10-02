c315a3baae2a7bef84c48e87a02f587197c5ce7f
Jangal Mangal <pycharm@0422>
2019-10-02 17:27:47 +0530

    solution of exercise1

--- solution1.py
+++ solution1.py
@@ -2,27 +2,33 @@
 def turn_100(m):
     if len(m)<=3:
         at_100 = 100 - int(m)
-
         if at_100 > 100:
-            print("You already lived more than a century")
-        elif at_100 < 0:
             print("You are not born yet")
+
+        elif at_100 < 0:
+            print("You already lived more than a century")
         else:
             if int(m) > 125:
                 print("You seems to be the oldest person alive :")
-            print("You will turn 100 after ")
-            return at_100
+            print(f"You will turn 100 after {at_100} years old")
 
     elif len(m)==4:
         at_100 = (int(m) + 100) - 2019
-        print("You will turn 100 after")
-        return at_100
+        if at_100 > 100:
+            print("You are not born yet :")
+        elif at_100<0:
+            print("You already lived more than a century:")
+        else:
+            print(f"You will turn 100 after {at_100} years old")
+
 
     else:
         print("You entered wrong input")
 
 def future_age(m):
-    f = int(input("Enter the year you want to find your age:"))
+    if len(m) == 4:
+        b = int(input("Enter the year when you want to find your age:"))
+        print(f"You will be {b - int(m)} years old")
 
 
 if __name__ == "__main__":
@@ -35,7 +41,5 @@ if __name__ == "__main__":
         m = input("Enter you birth year:")
     else:
         print("Enter 1 or 2 only ")
-    result = turn_100(m)
-    print(result)
-    print()
-
+    result1 = turn_100(m)
+    result2 = future_age(m)
