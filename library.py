e48c160010e6b9d34cdb63d205a75449bd07f3f0
Faraz Khan <pycharm0422@gmail.com>
2019-10-06 12:18:19 +0530

    upgraded version of previous library

--- library.py
+++ library.py
@@ -12,10 +12,13 @@ class Library:
 
     def lend(self, user, book):
         if book not in self.lended_book.keys():
-            print("Book is available ")
-            self.lended_book.update({book:user})
-            print("Database is updated ")
-            self.book.remove(book)
+            if book not in self.book:
+                print(f"Sorry we dont have {book} book:\n")
+            else:
+                print("Book is available ")
+                self.lended_book.update({book:user})
+                print("Database is updated ")
+                self.book.remove(book)
         else:
             print("Enter the book present in library : ")
 
@@ -37,31 +40,38 @@ if __name__ == "__main__":
         print("2: To lend the book : ")
         print("3: To add the book to library : ")
         print("4: To return the book to library: ")
-        n = int(input("Enter the following number to perform the task :"))
-
+        try:
+            n = int(input("Enter the following number to perform the task :"))
+        except:
+            print("wrong input")
+            continue
 
         if n == 1:
             print("The books available are :")
             l1.show_book()
 
-        if n == 2:
+        elif n == 2:
             user = input("Enter the name of the user :")
             lends = input("Enter the book you want to lend :")
             l1.lend(user ,lends)
 
-        if n == 3:
+        elif n == 3:
             add_book = input("Enter the book you want to donate to library :")
             l1.add(add_book)
 
-        if n == 4:
+        elif n == 4:
             returns = input("Enter the book you want to return : ")
             l1.returns(returns)
+        else:
+            print("Give correct integer ")
 
         print("enter 'q' to quit : ")
-        print("enter 'c' to continue : ")
+        print("enter any button to continue: ")
         m = input()
         if m == 'q':
+            print("Thanks for visiting our library :")
             quit()
         if m == 'c':
             continue
+
 
