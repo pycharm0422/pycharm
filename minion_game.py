398348bb19ad535d5ca2d99cc16b474dbe217566
Faraz Khan <pycharm0422@gmail.com>
2019-10-09 23:21:27 +0530

    person with highest constraints or vowels wins

--- minion_game.py
+++ minion_game.py
@@ -1,30 +1,25 @@
-#
-# vowels = "AEIOU"
-# v = 0
-# nv = 0
-#
-# a = input("Enter any word (CAPS ON)")
-# l = list(a)
-# c = len(l)
-#
-# for j in range(len(a)):
-#     for i in range(len(a), 0, -1):
-#         # print(a[j:i])
-#         if a[j:i] == "":
-#             pass
-#         else:
-#             if a[j] in vowels:
-#                 v+= 1
-#             else:
-#                 nv+= 1
-# if v > nv:
-#     print("Faraz wins and point is {}".format(v))
-# else:
-#     print("Himanshu wins and point is {}".format(nv))
+
+vowels = "AEIOU"
+v = 0
+nv = 0
+
+a = input("Enter any word (CAPS ON)")
+l = list(a)
+c = len(l)
+
+for j in range(len(a)):
+    for i in range(len(a), 0, -1):
+        # print(a[j:i])
+        if a[j:i] == "":
+            pass
+        else:
+            if a[j] in vowels:
+                v+= 1
+            else:
+                nv+= 1
+if v > nv:
+    print("Faraz wins and point is {}".format(v))
+else:
+    print("Himanshu wins and point is {}".format(nv))
 
 
-for i in range(34):
-    print(i)
-
-r = list(i)
-print(r)
